88b1ef5df9313bfc6ed631d96dfb0fe9a72d35c6
Rrinors <2421469943@qq.com>
2023-07-31 00:49:14 +0800

    撤销移除 CInv

--- MPair.cpp
+++ MPair.cpp
@@ -108,6 +108,9 @@ struct MInt {
 template<>
 int MInt<0>::Mod = 1;
 
+template<int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+
 template<int P1, int P2>
 struct MPair {
     MInt<P1> x;
