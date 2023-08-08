10bccfaf59b80fc2162b0068c7acaf7df6779528
Rrinors <2421469943@qq.com>
2023-08-09 00:14:05 +0800

    修改 f

--- RMQ.cpp
+++ RMQ.cpp
@@ -1,16 +1,19 @@
-template<class T, auto f>
+template<class T>
 struct RMQ {
     std::vector<std::vector<T>> a;
-    RMQ(const std::vector<T> &b) {
-        init(b);
+    std::function<T(T, T)> f;
+
+    RMQ(std::vector<T> a_, std::function<T(T, T)> f_) {
+        init(a_, f_);
     }
     RMQ() {}
 
-    void init(const std::vector<T> &b) {
-        int n = b.size();
+    void init(std::vector<T> a_,  std::function<T(T, T)> f_) {
+        f = f_;
+        int n = a_.size();
         int s = 31 - __builtin_clz(n);
         a.assign(s + 1, std::vector<T>(n));
-        a[0] = b;
+        a[0] = a_;
         for (int i = 1; i <= s; i++){
             for (int j = 0; j + (1 << i) - 1 < n; j++){
                 a[i][j] = f(a[i - 1][j], a[i - 1][j + (1 << (i - 1))]);
@@ -18,7 +21,6 @@ struct RMQ {
         }
     }
     T query(int l, int r) {
-        assert(l <= r);
         int s = 31 - __builtin_clz(r - l + 1);
         return f(a[s][l], a[s][r - (1 << s) + 1]);
     }
