1b8b8a92d7f4f6a1f1fe1d1674c43b6a59e936b5
Rrinors <2421469943@qq.com>
2023-08-10 19:18:00 +0800

    新增 exkmp

--- KMP.cpp
+++ KMP.cpp
@@ -10,3 +10,23 @@ std::vector<int> kmp(const std::string &s) {
     }
     return next;
 }
+
+std::vector<int> exkmp(const std::string &s) {
+    int n = s.length();
+    std::vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
+        if (i <= r && z[i - l] < r - i + 1) {
+            z[i] = z[i - l];
+        } else {
+            z[i] = std::max(0, r - i + 1);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                z[i]++;
+            }
+        }
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
