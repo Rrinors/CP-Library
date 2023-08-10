0fc5f1cbf49d0f0db30eca91b6ea491ac92c2e73
Rrinors <2421469943@qq.com>
2023-08-10 18:10:23 +0800

    重构

--- KMP.cpp
+++ KMP.cpp
@@ -1,24 +1,12 @@
-
-    std::vector<int> next(m + 1);
-    for (int i = 1, j = 0; i < m; i++) {
-        while (j > 0 && t[i] != t[j]) {
+std::vector<int> kmp(const std::string &s) {
+    int n = s.length();
+    std::vector<int> next(n + 1);
+    for (int i = 1, j = 0; i < n; i++) {
+        while (j && s[i] != s[j]) {
             j = next[j];
         }
-        if (t[i] == t[j]) {
-            j++;
-        }
-        next[i + 1] = j; 
+        j += s[i] == s[j];
+        next[i + 1] = j;
     }
-
-    for (int i = 0, j = 0; i < n; i++) {
-        while (j > 0 && s[i] != t[j]) {
-            j = next[j];
-        }
-        if (s[i] == t[j]) {
-            j++;
-        }
-        if (j == m) {
-            std::cout << i - j + 2 << "\n";
-            j = next[j];
-        }
-    }
+    return next;
+}
