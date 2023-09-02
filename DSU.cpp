81299ad6c342e0d415811da1dfe6e9fb8403e3f0
Rrinors <2421469943@qq.com>
2023-09-02 16:57:33 +0800

    格式化

--- DSU.cpp
+++ DSU.cpp
@@ -1,30 +1,32 @@
 struct DSU {
     std::vector<int> f, siz;
+
+    DSU() {}
     DSU(int n) {
         init(n);
     }
-    DSU() {}
     
     void init(int n) {
-        siz.assign(n, 1);
         f.resize(n);
         std::iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
     }
-    int leader(int x) {
+
+    int find(int x) {
         while (x != f[x]) x = f[x] = f[f[x]];
         return x;
     }
     bool merge(int x, int y) {
-        x = leader(x), y = leader(y);
+        x = find(x), y = find(y);
         if (x == y) return false;
         siz[x] += siz[y];
         f[y] = x;
         return true;
     }
     bool same(int x, int y) {
-        return leader(x) == leader(y);
+        return find(x) == find(y);
     }
     int size(int x) {
-        return siz[leader(x)];
+        return siz[find(x)];
     }
 };
