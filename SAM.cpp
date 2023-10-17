eda25b3e1da2cc6ac9fae5c530efb16082ea4e93
Rrinors <2421469943@qq.com>
2023-10-17 17:37:36 +0800

    更改底层容器 vector->array

--- SAM.cpp
+++ SAM.cpp
@@ -1,44 +1,50 @@
-template<int ALPHABET_SIZE = 26>
+template<int ALPHABET = 26>
 struct SAM {
     struct Node {
-        int len;
-        int link;
-        std::array<int, ALPHABET_SIZE> next;
-        Node() : len{}, link{-1}, next{} {}
-    };
-    int last;
-    std::vector<Node> t;
-    SAM() { init(); }
+        int len, link;
+        std::array<int, ALPHABET> ch;
+        Node() : len{}, link{-1}, ch{} {}
+    } t[2 * N];
+
+    int last, tot;
+
+    SAM() {
+        init();
+    }
 
     void init() {
         last = 0;
-        t.assign(1, Node());
+        tot = 0;
+        t[0] = Node();
     }
+
     int newNode() {
-        t.emplace_back();
-        return t.size() - 1;
+        tot++;
+        t[tot] = Node();
+        return tot;
     }
+
     int extend(int c) {
         int cur = newNode();
         t[cur].len = t[last].len + 1;
         int p = last;
-        while (p != -1 && !t[p].next[c]) {
-            t[p].next[c] = cur;
+        while (p != -1 && !t[p].ch[c]) {
+            t[p].ch[c] = cur;
             p = t[p].link;
         }
         if (p == -1) {
             t[cur].link = 0;
         } else {
-            int q = t[p].next[c];
+            int q = t[p].ch[c];
             if (t[p].len + 1 == t[q].len) {
                 t[cur].link = q;
             } else {
                 int clone = newNode();
                 t[clone].len = t[p].len + 1;
-                t[clone].next = t[q].next;
+                t[clone].ch = t[q].ch;
                 t[clone].link = t[q].link;
-                while (p != -1 && t[p].next[c] == q) {
-                    t[p].next[c] = clone;
+                while (p != -1 && t[p].ch[c] == q) {
+                    t[p].ch[c] = clone;
                     p = t[p].link;
                 }
                 t[q].link = t[cur].link = clone;
@@ -46,24 +52,24 @@ struct SAM {
         }
         return last = cur;
     }
-    
-    int len(int x) {
+
+    int length(int x) const {
         return t[x].len;
     }
-    int link(int x) {
+
+    int fail(int x) const {
         return t[x].link;
     }
-    int next(int x, int c) {
-        return t[x].next[c];
+
+    int next(int x, int c) const {
+        return t[x].ch[c];
     }
-    int size() {
-        return t.size();
-    }
-    std::vector<std::vector<int>> getTree() {
-        std::vector<std::vector<int>> adj(size());
-        for (int i = 1; i < t.size(); i++) {
-            adj[link(i)].push_back(i);
-        }   
+
+    std::vector<std::vector<int>> getTree() const {
+        std::vector<std::vector<int>> adj(tot + 1);
+        for (int i = 1; i <= tot; i++) {
+            adj[t[i].link].push_back(i);
+        }
         return adj;
     }
 };
